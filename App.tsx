0303603b8b225abd028245bf007f36f11d29ab37
Thiago Alencar <thiago.alencar@pontomais.com.br>
2022-05-20 21:36:39 -0300

    :sparkles: Native Base layout

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StatusBar } from "react-native";
 
-import AppLoading from "expo-app-loading";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 
 import { Routes } from "./src/routes";
+
+import AppLoading from "expo-app-loading";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_700Bold,
-
 } from "@expo-google-fonts/poppins";
 
 export default function App() {
@@ -23,8 +24,10 @@ export default function App() {
 
   return (
     <>
-      <StatusBar barStyle={"light-content"} backgroundColor="#991F36" />
-      <Routes />
+      <StatusBar barStyle={"light-content"} backgroundColor="#350693" />
+      <NativeBaseProvider>
+        <Routes />
+      </NativeBaseProvider>
     </>
   );
 }
